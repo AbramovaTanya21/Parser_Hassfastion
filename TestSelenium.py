17095c77e70e1b2ad17a0e4e808a3b7b74208034
AbramovaTanya21 <123319691+AbramovaTanya21@users.noreply.github.com>
2025-03-20 18:52:44 +0300

    Add files via upload

--- TestSelenium.py
+++ TestSelenium.py
@@ -25,39 +25,31 @@ def AuthorizationForHasfesshen(driver):
     Auth_Imp.click()
     
     time.sleep(10) 
-    
-#Получение страниц каталога
+        
+#Получение страниц каталога из Экселя
 def GettingСategories(driver):
-   
     # Обращение к эксель файлу
-    file_name = "HassCatalog.xlsx"
-    file_path = "G:\\NRU\\SP\\Parsing\\TestSelenium\\TestSelenium\\HassCatalog.xlsx" 
-   
+    file_path = "G:\\NRU\\SP\\Parsing\\TestSelenium\\TestSelenium\\HassCatalog.xlsx"  
     wb = load_workbook(file_path)
     ws = wb.active 
     LinkPages = []
-    
-
     for index, row in enumerate(range(2, ws.max_row + 1)):
-         current_collection = ws[row][0].value 
-         if index > 0:
+        print(row, index) 
+        current_collection = ws[row][0].value 
+        if index > 0:
             last_collection = ws[row - 1][0].value  
-            if last_collection !=  current_collection:
-                print(f'Start parsing page for next colltction: {last_collection}'  )
-                Goods = ParsingPage(driver, LinkPages) 
-                RecordingInExcel(driver,Goods,last_collection)
-                LinkPages = []              
-         LinkPages.append(ws[row][2].value) 
-         
+            if last_collection !=  current_collection and last_collection !=  None :
+                    print(f'Start parsing page for next colltction: {last_collection}')
+                    Goods = ParsingPage(driver, LinkPages) 
+                    RecordingInExcel(driver,Goods,last_collection)
+                    LinkPages = []              
+        LinkPages.append(ws[row][2].value) 
+    lastest_collection = ws[ws.max_row][0].value
+    print(f'Start parsing page for next colltction: {lastest_collection}')
     Goods = ParsingPage(driver, LinkPages) 
-    RecordingInExcel(driver,Goods,last_collection)
+    RecordingInExcel(driver,Goods,lastest_collection)   
     
-#Парсинг страницы
-def ParsingPage(driver, LinkPages):    
-  for LinkPage in LinkPages:   
-      driver.get(LinkPage) 
-      time.sleep(10)
-      class TabInd:
+class TabInd:
             NAME = 0
             ARTICLE = 1
             BRAND = 2
@@ -66,14 +58,23 @@ def ParsingPage(driver, LinkPages):
             DESCRIPTION = 5
             PHOTO = 6
             LINK = 7
+    
+#Парсинг страницы
+def ParsingPage(driver, LinkPages): 
+  Goods = [] 
+  Video = []
+  print(LinkPages)
+  for LinkPage in LinkPages:   
+      driver.get(LinkPage) 
+      print(LinkPage)
+      time.sleep(10)  
       # Отбор товаров в наличие
       Links = [] 
       OnSales = driver.find_elements(By.XPATH, "//div[@class='res_cards']/div[not(@style)]") 
       for OnSale in OnSales:   
          a = OnSale.find_element(By.TAG_NAME, "a")
          Links.append(a.get_attribute("href")) 
-      # Сбор данных со страницы товара в структуры и запись в список
-      Goods = []
+      # Сбор данных со страницы товара в структуры и запись в список 
       for Link in Links:
         driver.get(Link)         
         #Имя
@@ -85,10 +86,12 @@ def ParsingPage(driver, LinkPages):
         #Цена
         Price = driver.find_element(By.XPATH,"//span[@class='price']").get_attribute("innerText").strip("₽").replace("\xa0","")
         #Размер
-        Size = ""
+        SizeList = []
         Sizes = driver.find_elements(By.XPATH,"//div[contains(@class,'offer-size size df aic jcc txt_bolder')][not(contains(@class,'disable'))]")
-        for Siz in Sizes:      
-            Size += Siz.get_attribute("innerText") + " "         
+        if Sizes == []: Size = "-"
+        else:
+            for Siz in Sizes: SizeList.append(Siz.get_attribute("innerText")) 
+            Size = ", ".join(SizeList) + "."    
         # Описание 
         Description = "" 
         symbols = "АВ:"
@@ -97,14 +100,19 @@ def ParsingPage(driver, LinkPages):
         try:
              Discript = driver.find_element(By.XPATH,"//span[@class='description_text txt typing']").get_attribute("innerText")   
         except:
-            Discript = " - "
+            Discript = " Состав: "
         Description = Discript + " " + Composition 
         #Картинки
         Picture = []
         Pictures = driver.find_elements(By.XPATH,"//div[@class ='miniatures_item df fdc']//img")
-        for index, Pict in enumerate(Pictures): 
-            if index % 2 != 0:
-                Picture.append(Pict.get_attribute("src"))
+        Video.append(Pictures[0].get_attribute("src"))
+        for index, Pict in enumerate(Pictures):
+            if index > 0:
+                 if len(Pictures) > 5:
+                    if index % 2 != 0:
+                         Picture.append(Pict.get_attribute("src"))
+                 else:
+                    Picture.append(Pict.get_attribute("src"))    
         # Запись данных в экземляр структуры StructureOfProducts    
         StructureOfProduct = {
             TabInd.NAME : Name,
@@ -118,121 +126,64 @@ def ParsingPage(driver, LinkPages):
             }
         # Запись структуры в список   
         Goods.append(StructureOfProduct)
-      print(Goods)
-      return Goods
-      RecordingInExcel(driver,Goods,CategoryName)
-    
+  return Goods 
+  return Video
+   
 #Запись в эксель      
 def RecordingInExcel(driver,Goods,CategoryName): 
     # Создание\загрузка эксель файла
-    file_name = "hasf-parser1.xlsx"
-    file_path = "G:\\NRU\\SP\\Parsing\\TestSelenium\\TestSelenium\\hasf-parser1.xlsx" 
+    file_name = "hasf-parser2.xlsx"
+    file_path = "G:\\NRU\\SP\\Parsing\\TestSelenium\\TestSelenium\\hasf-parser2.xlsx" 
     if os.path.exists(file_name):
         wb = load_workbook(file_path)
         print(f"Файл '{file_name}' успешно загружен.")
     else:
          wb = Workbook()  
          print(f"Файл '{file_name}' успешно создан.")     
-    # Обращение к книге и листам
+    # Обращение к книге и листам  
     if CategoryName in wb.sheetnames:
-        ws = wb[CategoryName]
+        #Обращение к листу и удаление не актуальных данных  
+        ws = wb[CategoryName] 
         print(f"Лист '{CategoryName}' уже существует. Данные будут обновлены.")
+        if ws.cell(row=2, column=1).value is not None:
+           for row in ws.iter_rows():
+               for cell in row: 
+                   cell.value = None   
     else:
+        #Создание листа 
         ws = wb.create_sheet(title=CategoryName)
         print(f"Создан новый лист '{CategoryName}'.")
-       
-    # Добавление данных
-        headers = ['Название', 'Артикл', 'Бренд', 'Цена', 'Размер', 'Описание', 'Изображение', 'Изображение1', 'Изображение2', 'Изображение3', 'Ссылка на товар']
-        ws.append(headers)
     
-    # Определение активную строки заполнения    
-    start_row = ws.max_row + 1  
-    # for row_idx in range(1, ws.max_row + 1):
-    #      row_values = [cell.value for cell in ws[row_idx]]
-    #      if not any(row_values):  
-    #           start_row = row_idx
-    #           break
-            
-    for index, item in enumerate(Goods, start=start_row):  
+    # Добавление данных
+    headers = ['Название', 'Артикл', 'Бренд', 'Цена', 'Размер', 'Описание', 'Изображение', 'Изображение1', 'Изображение2', 'Изображение3', 'Ссылка на товар']  
+    for col_num, header in enumerate(headers, start=1):
+         ws.cell(row=1, column=col_num, value=header)
+    
+    for index, item in enumerate(Goods, start=2):  
         ws.cell(row=index, column=1, value=item[0])  
         ws.cell(row=index, column=2, value=item[1])
         ws.cell(row=index, column=3, value=item[2])
         ws.cell(row=index, column=4, value=item[3])
         ws.cell(row=index, column=5, value=item[4])
         ws.cell(row=index, column=6, value=item[5])
-        for img_index, img in enumerate(item[6]):
-            if img_index < 4:
-                ws.cell(row=index, column=7 + img_index, value=img) 
+        for imgindex, img in enumerate(reversed(item[6])):   
+             if imgindex < 4:
+                ws.cell(row=index, column=7 + imgindex, value = img) 
         ws.cell(row=index, column=11, value=item[7])
-       
     wb.save(file_path)
     print(f"Данные по категории {CategoryName} успешно записаны в файл '{file_name}'.") 
     
     try:  
-        FL = wb["Sheet"]
-        FL.title = "Лист1"
-        wb.remove(wb["Лист1"])
-        # wb.move_sheet(wb["Лист1"], offset = len(wb.sheetnames) - 1)
+        wb["Sheet"].title = "Лист1"
+        wb.remove(wb["Лист1"])     
         wb.save(file_path)   
-    except:
-        pass
-     
+    except: pass 
+    
 #Главная процедура   
-s=Service('G:\\NRU\\SP\\Parsing\\selenium\\chromedriver\\win64\\130.0.6723.69\\chromedriver-win64\\chromedriver.exe')
+s=Service('G:\\NRU\\SP\\Parsing\\selenium\\chromedriver\\win64\\134.0.6998.88\\chromedriver-win64\\chromedriver.exe')
 driver = webdriver.Chrome(service=s)
 AuthorizationForHasfesshen(driver)
 GettingСategories(driver)
-
 input()
 
-    ## file_puth = "G:\NRU\SP\Parsing\TestSelenium\TestSelenium\hasf-parser.xlsx"
-    # wb = openpyxl.load_workbook(file_puth) 
-    # Auht_page = driver.current_window_handle
-    # print(Auht_page)
-    # driver.window_handles
-    # driver.switch_to.window()
-
-    #
-    # password = driver.find_element(By.NAME, "password")
-    # password.send_keys('k39cisdjt0_nj')
-    # time.sleep(2)
-
-    # sabmit = driver.find_elements(By.TAG_NAME, "input")
-    # sabmit[0].click()
-
-    # print(login)
-
-
-
-
-# Auth = driver.find_element(By.CLASS_NAME, 'btn_DaWUW')
-# Auth.click()
-# Auth2 = driver.find_element(By.CLASS_NAME, 'content_A9YMTcontent_A9YMT')
-# Auth2.click()
-
-
-
-
-# password = driver.find_element(By.CLASS_NAME, 'text-style-ui-body input_Eg63j')
-# password.send_keys_for_element('k3956577')
-
-# sabmit = driver.find_element(By.CLASS_NAME, 'class="text-style-ui-body-bold uiButton_A9YMT primary_A9YMT large_A9YMT"')
-# sabmit.click()
-
-# time.sleep(3)
-
-# # driver.quit()
-# driver.get("https://www.nn.ru/")
-
-
-# time.sleep(2)
-# login = driver.find_element(By.CLASS_NAME, 'text-style-ui-body input_Eg63j')
-# login.send_keys_for_element('Olg')
-
-# password = driver.find_element(By.CLASS_NAME, 'text-style-ui-body input_Eg63j')
-# password.send_keys_for_element('k3956577')
-
-# sabmit = driver.find_element(By.CLASS_NAME, 'class="text-style-ui-body-bold uiButton_A9YMT primary_A9YMT large_A9YMT"')
-# sabmit.click()
-
-# time.sleep(3)
+    
